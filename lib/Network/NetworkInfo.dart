13f4dd1248b9498cea573c3a9d5c70ca91003213
sai kiran <104380617+saikiranbanka749@users.noreply.github.com>
2024-06-13 11:44:11 +0530

    Update NetworkInfo.dart

--- lib/Network/NetworkInfo.dart
+++ lib/Network/NetworkInfo.dart
@@ -1,4 +1,4 @@
 class NetworkInfo {
   //static String url2 = "http://192.168.2.142/allowMe/";
-  static String url2 = "http://192.168.172.187/allowMe";
+  static String url2 = "http://13.233.116.233/allowMe";
 }
